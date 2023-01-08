2e59f99854fdf49eb42e61eaca4a3cce6bc8ab58
Maryam313 <119934446+Maryam313@users.noreply.github.com>
2023-01-08 14:47:47 +0400

    Added file

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Test"},{"p":"TicTacToeClass","l":"TicTac"},{"p":"<Unnamed>","l":"TicTacToe"}];updateSearchResults();
